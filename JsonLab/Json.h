dc01dbec2c66df8ca5ef1545cab8eca2d08644aa
Никита Кораблев <kit06092003@gmail.com>
2023-04-03 22:32:16 +0300

    aga

--- JsonLab/Json.h
+++ JsonLab/Json.h
@@ -7,6 +7,8 @@
 //#define MY_DLL_JSON __declspec(dllexport)
 //#endif
 
+//---------------------------
+// Что это? 
 namespace JsonLib {
 	public ref class Json {
 	public:
@@ -15,3 +17,51 @@ namespace JsonLib {
 		}
 	};
 }
+//---------------------------
+
+
+#include "string"
+
+using namespace std;
+
+class IterValue {};
+
+class IValue {
+public:
+    virtual int getType() = 0;
+    virtual IterValue* itr() = 0;
+    virtual string getKey() = 0;
+    virtual string getVal() = 0;
+};
+
+class strValue : public IValue {
+    string key;
+    string val;
+public:
+
+};
+
+struct Link {
+    Link* next;
+    IValue* val;
+};
+
+class listValue : public IValue {
+    Link* n;
+    string key;
+public:
+
+};
+
+class JSon {
+    listValue* root;
+public:
+    void load(string filename) {};
+    void save(string filename) {};
+
+    void next(); // идём дальше
+    void down(); // идём в глубь
+    void beak(); // идём наверх
+};
+
+
